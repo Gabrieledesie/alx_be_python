630de091e0377a2e473b68374204d1d26172e872
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-06-22 15:23:24 +0100

    Fix: Add __str__ method to Book, EBook, and PrintBook classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,12 @@
+# library_system.py
+
 # Base class
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -14,7 +16,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_info(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -24,11 +26,11 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Composition class
+# Library using composition
 class Library:
     def __init__(self):
         self.books = []
@@ -38,4 +40,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)  # This will use __str__()
